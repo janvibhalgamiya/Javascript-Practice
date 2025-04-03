05fa6a5923aa343c8cfb6e2407cb4191956ee655
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-04-03 14:47:40 +0530

    ternary operator

--- even-odd.js
+++ even-odd.js
@@ -1,11 +1,17 @@
 // write a function that takes a number and returns "even" or "odd"
 
 function evenodd(num) {
-  if (num % 2) {
-    return "Odd Number";
-  } else {
-    return "Even Number";
-  }
+  //   if (num % 2) {
+  //     return "Odd Number";
+  //   } else {
+  //     return "Even Number";
+  //   }
+  // ternary operator:
+ return ((num % 2 === 0) ? "Odd Number" : "Even Number");
 }
+
+// difference between == and ===
+console.log("----",123 == "123")
+console.log("----",123 === "123") //third equal check the type
 
 console.log(evenodd(253));

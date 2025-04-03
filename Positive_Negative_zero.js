36eacf29b7135c374775bb46afb5094b37ab920f
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-04-03 16:17:36 +0530

    code convert is small

--- Positive_Negative_zero.js
+++ Positive_Negative_zero.js
@@ -1,12 +1,13 @@
 //checks if the number is positive,negative or zero.
 
 function CheckNumber(num){
-    if(num > 0){
-        return "The Number is Positive";
-    }else if(num < 0){
-        return "The Number is Negative";
-    }else{
-        return "The number is Zero";
-    }
+    // if(num > 0){
+    //     return "The Number is Positive";
+    // }else if(num < 0){
+    //     return "The Number is Negative";
+    // }else{
+    //     return "The number is Zero";
+    // }
+    return((num > 0) ? "The Number is Positive" : (num < 0) ? "The Number is Negative" : "The number is Zero")
 }
-console.log(CheckNumber(0));
+console.log(CheckNumber(-5));

40d10d99b0071db254d859c975990f861086fec3
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-04-03 15:50:06 +0530

    Array print by for loop

---
+++ Array-Loop.js
@@ -0,0 +1,9 @@
+const fruits = ["mango", "orange", "banana", "grapes"];
+// console.log(fruits[2]);
+
+
+for(let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+    // console.log(i);
+    
+}

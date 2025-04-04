b0335ec7aa2770aa0771b0be220f7989f4b6e490
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-04-04 09:58:01 +0530

    ternary operator

--- Multiple_Number.js
+++ Multiple_Number.js
@@ -1,10 +1,11 @@
 // Checks if the number is a multiple of 5 & logs "High Five!" if it is
 
 function MultipleNum(num){
-if(num % 5 === 0){
-    return "High Five..!😊"
-}else{
-    return "The Number not multiple by 5";
-}
+// if(num % 5 === 0){
+//     return "High Five..!😊"
+// }else{
+//     return "The Number not multiple by 5";
+// }
+return ((num % 5 === 0) ? "High Five...!😊" : "The Number not multiple by 5");
 }
 console.log(MultipleNum(2));

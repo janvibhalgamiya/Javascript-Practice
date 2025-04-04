a478f999ec4b831536b66998eefd38ae131506e6
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-04-04 10:59:26 +0530

    String beginner task

---
+++ string_tasks.js
@@ -0,0 +1,44 @@
+//? Template literal & String tasks
+
+//*Hello,who dis?
+
+//Take a name as a variable & log this
+
+function MyName(name){
+    return(`Hello ${name}! Welcome to Javascript`)
+}
+console.log(MyName("Janvi"));
+
+//**************************************************************************************** */
+
+//* Age reveal Drama
+
+//create variable  name & age,then log
+
+function NameAge(name,age){
+    return(`Hi, I'm ${name} and I'm ${age} years old. Totally becoming a js pro`)
+}
+console.log(NameAge("Janvi",19));
+
+//**************************************************************************************** */
+
+//*sum it up like pro.
+
+//given two numbers,log
+
+// function Sum(a,b,c=a+b){
+function Sum(a,b){
+    return (`The sum of ${a} and ${b} is ${a + b}`)
+}
+console.log(Sum(23,3));
+
+//**************************************************************************************** */
+
+//*String length
+
+//Take a username variable & log
+
+function UserName(name){
+    return(`Your Username has ${name.length} characters`)
+}
+console.log(UserName("Janvi@2006"));

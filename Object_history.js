2b2375525eb08da5a045ddc6e96e3a8b78a0ca12
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-04-16 12:37:55 +0530

    Other Variants

--- Object_history.js
+++ Object_history.js
@@ -330,7 +330,22 @@
 //       alert("I am admin");
 //     }
 //   };
-  
 //   let userGuest = {};
 //   userAdmin.admin?.();   //* I am admin
 //   userGuest.admin?.();   //* nothing happens (no such method)
+
+
+
+// let key = "firstName";
+// let user1 = {
+//   firstName: "John"
+// };
+// let user2 = null;
+// alert( user1?.[key] );      //* John
+// alert( user2?.[key] );      //* undefined
+
+
+//? delete varient
+
+// delete user?.name;    //* delete user.name if user exists
+

22ff07764bf233cf2804d53c871026fee84bcaa0
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-04-16 15:41:10 +0530

    Symbol type

--- Object_history.js
+++ Object_history.js
@@ -349,3 +349,85 @@
 
 // delete user?.name;    //* delete user.name if user exists
 
+
+
+
+//? ******** Symbol Type **********
+
+//? Symbols (A “symbol” represents a unique identifier.)
+
+// let id1 = Symbol("id");
+// let id2 = Symbol("id");
+// alert(id1 == id2);          //* false
+
+
+
+//? Hidden Properties
+
+// let user = {                     //* belongs to another code
+//     name: "John"
+//   };
+  
+//   let id = Symbol("id");
+//   user[id] = 1;
+//   alert( user[id] );             //* we can access the data using the symbol as the key
+
+
+
+//? symbol in an oobject literal (If we want to use a symbol in an object literal {...}, 
+//?                                    we need square brackets around it.)
+
+// let id = Symbol("id");
+
+// let user = {
+//   name: "John",
+//   [id]: 123              //* not "id": 123
+// };
+
+
+
+//? symbols are skipped by for..in (Symbolic properties do not participate in for..in loop.)
+
+// let id = Symbol("id");
+// let user = {
+//   name: "John",
+//   age: 30,
+//   [id]: 123
+// };
+
+// for (let key in user) alert(key);             //* name, age (no symbols)
+// //* the direct access by the symbol works
+// alert( "Direct: " + user[id] );               //* Direct: 123
+
+
+
+//? Global symbols
+
+//* read from the global registry
+// let id = Symbol.for("id");           //* if the symbol did not exist, it is created
+
+//* read it again (maybe from another part of the code)
+// let idAgain = Symbol.for("id");
+
+//* the same symbol
+// alert( id === idAgain );            //* true
+
+
+
+//? Symbol.KeyFor
+
+//* get symbol by name
+// let sym = Symbol.for("name");
+// let sym2 = Symbol.for("id");
+//* get name by symbol
+// alert( Symbol.keyFor(sym) );     //* name
+// alert( Symbol.keyFor(sym2) );    //* id
+
+
+//? System symbol
+
+// Symbol.hasInstance
+// Symbol.isConcatSpreadable
+// Symbol.iterator
+// Symbol.toPrimitive
+// …and so on...?

888acb0e64f9414e1d54da615a61a5c432d16b42
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-04-17 10:13:18 +0530

    Method of primitive

---
+++ Data_Type.js
@@ -0,0 +1,20 @@
+//? *******************Methods of primitive************************
+
+//? There are 7 primitive types: string, number, bigint, boolean, symbol, null and undefined.
+
+//? &&&&&&&&&&&&&&&&&&&&&  String Method &&&&&&&&&&&&&&&&&&&&
+
+//? str.toUpperCase()    (capital karva mate)
+
+// let str = "Hello";
+// alert( str.toUpperCase() );       //* HELLO
+
+
+
+//? toFixed(n)    (point pachi ketla number mukva ani mate)
+
+// let n = 1.23456;
+// alert( n.toFixed(2) ); // 1.23
+
+
+

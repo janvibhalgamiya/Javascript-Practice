d27cc213a39f494c63aaba814d0b0e46accb9e83
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-05-15 14:41:17 +0530

    small changes

--- even-odd.js
+++ even-odd.js
@@ -1,13 +1,13 @@
 // write a function that takes a number and returns "even" or "odd"
 
 function evenodd(num) {
-  //   if (num % 2) {
-  //     return "Odd Number";
-  //   } else {
+  //   if (num % 2) { 
   //     return "Even Number";
+  //   } else {
+  //     return "Odd Number";
   //   }
   // ternary operator:
- return ((num % 2 === 0) ? "Odd Number" : "Even Number");
+ return ((num % 2 === 0) ? "Even Number" : "Odd Number");
 }
 
 // difference between == and ===
@@ -15,3 +15,4 @@ console.log("----",123 == "123")
 console.log("----",123 === "123") //third equal check the type
 
 console.log(evenodd(253));
+
